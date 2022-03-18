e1dd51db6c6482ef5264d522807e1fa4e5fb93ba
Breanne <BreMarotta@gmail.com>
2022-03-18 14:41:16 -0400

    add some basic formatting for cards in collection

--- src/Components/LegoSet.js
+++ src/Components/LegoSet.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const LegoSet = () => {
+const LegoSet = ({ set }) => {
+  const cardStyles = {
+    background: "whitesmoke",
+    paddingLeft: "3px",
+    display: "inline-grid",
+    margin: ".5rem .5rem",
+    width: "150px",
+    height: "250px"
+  }
+
   return (
-    <div>LegoSet</div>
+    <div style={cardStyles}>
+      <div className="image" style={{width: "100"}}>
+          <img src={set.img} alt="No Image Available" />
+        </div>
+        <div className="content">
+          <p>{set.name}</p>
+          <p>Pieces: {set.pieces}</p>
+          <p>Set Number: {set.set_number}</p>
+        </div>
+    </div>
   )
 }
 
